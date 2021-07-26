dde7f03c8d4c4e23eabe9a46ed3cc1e4f75ac552
njei nicodona <njeinicodona@gmail.com>
2021-07-26 21:14:18 +0100

    this is a new repo with c function header

---
+++ README.md
@@ -0,0 +1,4 @@
+## printf Function for C 
+this is a printf function witha header file and C code
+
+
---
+++ holberton.h
@@ -0,0 +1,8 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#endif
